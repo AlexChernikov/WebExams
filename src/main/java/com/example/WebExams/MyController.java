fe57da1d01e53d21f68d33042fc080a37ff44ccf
Aleksandr Chernikov <aleksandrchernikov98@gmail.com>
2019-12-03 04:37:07 +0300

    Project complete

--- src/main/java/com/example/WebExams/MyController.java
+++ src/main/java/com/example/WebExams/MyController.java
@@ -3,95 +3,74 @@ package com.example.WebExams;
 import com.example.WebExams.ExamClasses.Exam;
 import com.example.WebExams.Parsers.ExamParser;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
 
 @RestController
 public class MyController {
     private static HashMap<String, Exam> exams = ExamParser.parse();
-
-    /*@GetMapping("/greeting")
-    @ResponseBody
-    public String greeting(@RequestParam String id) {
-        return "hello ID: " + id;
-    }*/
-    @GetMapping("/personId")
-    public List<String> personId(@RequestParam(required=false, name="id") String id) {
-        System.out.println("THERE " + id);
-        if (id != null) {
-            return List.of("Your id is: " + id);
-        } else {
-            return List.of("Write id in request (?id=)");
-        }
-    }
-
-    @GetMapping("/listProducts")
-    public List<Product> listProducts() {
-        return products;
-    }
+   private static ArrayList<Result> results = new ArrayList<>();
 
     @GetMapping("/exams")
-    public Set<String> getExams() {
+    public  Collection<Exam> getExams() {
+        return exams.values();
+    }
+
+    @GetMapping("/examsNames")
+    public Set<String> getExamsNames() {
         return exams.keySet();
     }
 
-    List<Product> products = Collections.synchronizedList(new ArrayList<>());
-
-    {
-        products.add(new Product("Product1", 1, true));
-        products.add(new Product("Product2", 10, true));
-        products.add(new Product("Product3", 100, false));
+    @PostMapping("/postResult")
+    public void postResult(@RequestBody Result result) {
+        results.add(result);
+        for (Result resultFromList : results) {
+            System.out.println(resultFromList);
+        }
     }
-
 }
 
-class Product {
+class Result{
+    String examName;
+    Integer trueAns;
+    String id;
 
-    private String name;
-    private int price;
-    private boolean inStock;
-
-    public Product(String name, int price, boolean inStock) {
-        this.name = name;
-        this.price = price;
-        this.inStock = inStock;
+    public String getExamName() {
+        return examName;
     }
 
-    public Product() {
+    public void setExamName(String examName) {
+        this.examName = examName;
+    }
+
+    public Integer getTrueAns() {
+        return trueAns;
+    }
+
+    public void setTrueAns(Integer trueAns) {
+        this.trueAns = trueAns;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     @Override
     public String toString() {
-        return "Product{" +
-                "name='" + name + '\'' +
-                ", price=" + price +
-                ", inStock=" + inStock +
+        return "Result{" +
+                "examName='" + examName + '\'' +
+                ", trueAns=" + trueAns +
+                ", id='" + id + '\'' +
                 '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public boolean isInStock() {
-        return inStock;
-    }
-
-    public void setInStock(boolean inStock) {
-        this.inStock = inStock;
     }
 }
